1d165b3752587f7a49ad51537f33547ca350ffb3
Somrat Hossain <somrat945hossain@gmail.com>
2023-06-25 16:41:14 +0600

    create dummy data for calender page

---
+++ lib/ui/data/dummy_calender_data.dart
@@ -0,0 +1,16 @@
+class DummyCalnederData{
+  int ?date;
+  String ?day;
+  bool ?isTap; 
+  DummyCalnederData(this.date, this.day, this.isTap);
+}
+
+List<DummyCalnederData> myCalenderData = [
+  DummyCalnederData(04, 'THU', false),
+  DummyCalnederData(05, 'FRI', false),
+  DummyCalnederData(06, 'SAT', false),
+  DummyCalnederData(07, 'SUN', false),
+  DummyCalnederData(08, 'MON', false),
+  DummyCalnederData(09, 'TUS', false),
+  DummyCalnederData(010, 'WED', false),
+];
