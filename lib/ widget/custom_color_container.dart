5502542d4cb579b503a66dd650f9085f89ff3662
dev-17 <bdmicrosolutions@gmail.com>
2023-06-25 11:18:02 +0600

    project page done

---
+++ lib/ widget/custom_color_container.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+myCustomColorContainer(Color color){
+  return Container(
+    height: 50,
+    width: 50,
+  );
+}
+
+List<Widget> myContainer = [
+  Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.blue,
+    ),
+  ), Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffF79293),
+    ),
+  ),
+  Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffFFBE3C),
+    ),
+  ), Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xff9C67F9),
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xff76BBAA),
+    ),
+  ),
+Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffE6EBF8),
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffFEEBF5),
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffFEEBF5),
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffFEEBF5),
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffFEEBF5),
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.lightGreen,
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.red,
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.green
+    ),
+  ),Container(
+    height: 50,
+    width: 50,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.grey,
+    ),
+  ),
+
+
+
+
+];
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:somrat/%20widget/demo_refresh_data.dart';
 import 'package:somrat/%20widget/uft_practice.dart';
 import 'package:somrat/ui/navBar/home_page.dart';
 import 'package:somrat/ui/navBar/navbar.dart';
+import 'package:somrat/ui/navBar/project.dart';
 import 'package:somrat/ui/on_boarding1.dart';
 import 'package:somrat/ui/qrScanner/scanner.dart';
 import 'package:somrat/ui/splash_screen.dart';
 import 'package:somrat/ui/team_member.dart';
+
+import ' widget/custom_project_deatils.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,11 +32,11 @@ class MyApp extends StatelessWidget {
 
         // Define the default `TextTheme`. Use this to specify the default
         // text styling for headlines, titles, bodies of text, and more.
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        ),
+        // textTheme: const TextTheme(
+        //   displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        //   titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        //   bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        // ),
       ),
       debugShowCheckedModeBanner: false,
       home: NavBar(),
