2d23768a93eca58fc4aaf35bcdac9674c2ae5f51
Somrat Hossain <somrat945hossain@gmail.com>
2023-06-25 13:28:56 +0600

    height and color change

--- lib/ widget/custom_color_container.dart
+++ lib/ widget/custom_color_container.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 
 myCustomColorContainer(Color color){
   return Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
   );
 }
 
 List<Widget> myContainer = [
   Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Colors.blue,
     ),
   ), Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
@@ -26,24 +26,24 @@ List<Widget> myContainer = [
     ),
   ),
   Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Color(0xffFFBE3C),
     ),
   ), Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Color(0xff9C67F9),
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
@@ -51,76 +51,85 @@ List<Widget> myContainer = [
     ),
   ),
 Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Color(0xffE6EBF8),
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
-      color: Color(0xffFEEBF5),
+      color: Color.fromARGB(255, 225, 198, 148),
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
-      color: Color(0xffFEEBF5),
+      color: Color.fromARGB(255, 249, 95, 177),
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
-      color: Color(0xffFEEBF5),
+      color: Colors.blueAccent,
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
-      color: Color(0xffFEEBF5),
+      color: Color.fromARGB(255, 144, 179, 126),
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Colors.lightGreen,
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Colors.red,
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Colors.green
     ),
   ),Container(
-    height: 50,
-    width: 50,
+    height: 20,
+    width: 20,
 
     decoration: BoxDecoration(
       shape: BoxShape.circle,
       color: Colors.grey,
+    ),
+  ),
+  Container(
+    height: 20,
+    width: 20,
+
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.pink,
     ),
   ),
 
