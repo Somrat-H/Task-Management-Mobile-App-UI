724f19e966428a17b148311f3f141cadd9a666db
Somrat Hossain <somrat945hossain@gmail.com>
2023-07-04 13:02:35 +0600

    just modify

--- lib/ widget/calender_tile.dart
+++ lib/ widget/calender_tile.dart
@@ -6,68 +6,74 @@ myCalenderTile() {
   return SizedBox(
     height: 120,
     width: double.infinity,
-    child: Stack(
-      children: [
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: SizedBox(
-              height: 110,
-              width: 350,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xff292E3B),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10, right: 16),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            myStyle('Meeting Discord', 16, Colors.white,
-                                FontWeight.w400),
-                            Container(
-                              height: 16,
-                              width: 16,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0xff00CC39),
-                              ),
-                            )
-                          ],
+    child: LayoutBuilder(
+      
+      builder: (context, constraint){
+      
+      return Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: SizedBox(
+                height: 110,
+                width: 350,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xff292E3B),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, right: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              myStyle('Meeting Discord', 16, Colors.white,
+                                  FontWeight.w400),
+                              Container(
+                                height: 16,
+                                width: 16,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xff00CC39),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      myStyle('Discuss Web Design Project', 12,
-                          const Color(0xffE9E9EB), FontWeight.w400),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      myStyle('10:00 AM - 10:30 AM', 14, Colors.white,
-                          FontWeight.w600),
-                    ],
+                        myStyle('Discuss Web Design Project', 12,
+                            const Color(0xffE9E9EB), FontWeight.w400),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        myStyle('10:00 AM - 10:30 AM', 14, Colors.white,
+                            FontWeight.w600),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-        Positioned(
-          top: 50,
-          left: 35,
-          child: Container(
-            height: 15,
-            width: 15,
-            decoration:
-                const BoxDecoration(shape: BoxShape.circle, color: Color(0xff9C67F9)),
-          ),
-        )
-      ],
+          Positioned(
+            top: 50,
+            left: 35,
+            child: Container(
+              height: 15,
+              width: 15,
+              decoration:
+                  const BoxDecoration(shape: BoxShape.circle, color: Color(0xff9C67F9)),
+            ),
+          )
+        ],
+      );
+      }
     ),
   );
 }
